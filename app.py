75f40362e692b087353d9984a0dd72b1df4badfe
kaif9711 <kaifmohammad2001@gmail.com>
2025-11-12 22:02:46 +0000

    Final test with user-managed SA

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Cloud'
+    return 'Hello Kaif....Welcome to Cloud'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
